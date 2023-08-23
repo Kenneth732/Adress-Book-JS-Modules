1aececa08c5c2b2f3a655448fe0d280ef1df1367
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-23 13:42:50 +0300

    handleFormSub

--- static/js/app.js
+++ static/js/app.js
@@ -5,3 +5,24 @@ const Contact = require('./contact');
 
 const addressBook = new AddressBook();
 
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const firstName = document.getElementById('firstNameInput').value;
+  const lastName = document.getElementById('lastNameInput').value;
+  const phoneNumber = document.getElementById('phoneInput').value;
+  const email = document.getElementById('emailInput').value;
+
+  const newContact = new Contact(firstName, lastName, phoneNumber, email);
+  addressBook.addContact(newContact);
+
+  document.getElementById('firstNameInput').value = '';
+  document.getElementById('lastNameInput').value = '';
+  document.getElementById('phoneInput').value = '';
+  document.getElementById('emailInput').value = '';
+
+  displayContacts(addressBook);
+}
+
+document.querySelector('#form').addEventListener('submit', handleFormSubmit);
+
